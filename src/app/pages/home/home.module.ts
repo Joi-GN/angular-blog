44b4cb7e40c640fbb1d01fa8d3e2b2a52d233275
Joice Gomes <devjoicegomes@gmail.com>
2023-05-05 19:35:49 -0300

    create home page module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { BigCardComponent } from 'src/app/components/big-card/big-card.component';
+import { SmallCardComponent } from 'src/app/components/small-card/small-card.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     CommonModule,
