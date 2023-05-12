52c7efd7d1adec8dbda416ee6d71de81f3f7108c
Joice Gomes <devjoicegomes@gmail.com>
2023-05-12 17:57:40 -0300

    add routes and routerLinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/posts/post/post.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) },
+  { path: 'post/:id', component: PostComponent, loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule)}
 ];
 
 @NgModule({
--- src/app/pages/posts/posts-routing.module.ts
+++ src/app/pages/posts/posts-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './posts.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [{ path: '', component: PostsComponent }];
+const routes: Routes = [
+  { path: '', component: PostsComponent }, 
+  { path: 'post/:id', component: PostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
