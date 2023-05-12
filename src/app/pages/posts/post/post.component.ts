400e68aaa67bfeb0cd402fade77ae2559c37f9e3
Joice Gomes <devjoicegomes@gmail.com>
2023-05-12 17:58:14 -0300

    update post component

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { posts } from 'src/app/data/posts';
+import { Post } from 'src/app/model/Post';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  id: string | null = '';
+  post: Post | undefined;
+
+  constructor( 
+    private route: ActivatedRoute,
+    private navigator: Router
+    ){}
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.paramMap.get('id');
+      this.post = this.getPostContent(this.id);
+      if (!this.post) this.navigator.navigate(['/not-found']);
+  }
+
+  getPostContent(id: string | null): Post | undefined {
+    return posts.find(post => post.id == parseInt(id!));
+  }
 }
