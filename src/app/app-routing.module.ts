f1b920ed1ed9671921aeb81551aa8eb6ec3141ba
Joice Gomes <devjoicegomes@gmail.com>
2023-05-12 19:24:20 -0300

    create not-found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/posts/post/post.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) },
-  { path: 'post/:id', component: PostComponent, loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule)}
+  { path: 'post/:id', component: PostComponent, loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HomeComponent } from './pages/home/home.component';
     SmallCardComponent,
     FooterComponent,
     HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
