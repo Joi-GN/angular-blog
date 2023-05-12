e323bb7d9fd029abccb7e6b96012dd38558eb57c
Joice Gomes <devjoicegomes@gmail.com>
2023-05-12 16:19:04 -0300

    delete home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', component: HomeComponent},
 ];
 
 @NgModule({
