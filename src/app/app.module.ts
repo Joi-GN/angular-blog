bf6232ae4300e768e1351f54408f4c9195dbd1e6
Joice Gomes <devjoicegomes@gmail.com>
2023-05-12 16:21:37 -0300

    update app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
     FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
